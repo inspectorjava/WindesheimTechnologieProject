c702bfda3dc776e8ce39e2f8ad7931dbf1a0ef88
Fabian van de Bor <info@fabianvdbor.nl>
2018-04-17 20:35:37 +0200

    Add project for the console application

---
+++ Console/src/Console.java
@@ -0,0 +1,25 @@
+
+/**
+ * Main class for the Console project.
+ */
+public class Console {
+
+    /**
+     * Private constructor.
+     */
+    private Console() {
+    }
+
+    /**
+     * main function.
+     * @param args default arguments
+     */
+    public static void main(final String[] args) {
+        System.out.println("[Console] Project loading.");
+
+        // Do whatever we need to do,
+        // its just a sample setup to make sure it works.
+
+        System.out.println("[Console] Project loaded.");
+    }
+}
