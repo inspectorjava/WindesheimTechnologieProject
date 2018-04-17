6312c755de6611d96cfd85872e917e9519fb8bc9
Zander van der Meer <zander.meer@gmail.com>
2018-04-17 09:10:33 +0200

    Initial commit

---
+++ src/main/java/Holtenbroek/Main.java
@@ -0,0 +1,14 @@
+package Holtenbroek;
+
+public class Main {
+
+    public static void main(String[] args)
+    {
+        System.out.println("[Holtenbroek] Project loading.");
+
+        // Do whatever we need to do, its just a sample setup to make sure it works.
+
+        System.out.println("[Holtenbroek] Project loaded.");
+    }
+
+}
---
+++ src/main/java/Marslanden/Main.java
@@ -0,0 +1,14 @@
+package Marslanden;
+
+public class Main {
+
+    public static void main(String[] args)
+    {
+        System.out.println("[Marslanden] Project loading.");
+
+        // Do whatever we need to do, its just a sample setup to make sure it works.
+
+        System.out.println("[Marslanden] Project loaded.");
+    }
+
+}
